a505973c16ff9a20563f9acaad3d57dbe6c473d8
Danilo Santos <danilosantosweb@hotmail.com>
2025-09-14 22:24:05 -0300

    Add new page and update layout

--- Home.py
+++ Home.py
@@ -3,6 +3,8 @@ import functions
 
 todos = functions.get_todos()
 
+st.set_page_config(layout="wide")
+
 def add_todo():
     todo = st.session_state["new_todo"].strip()
     if todo:
@@ -11,8 +13,11 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("This app is to increase your productivity.")
+st.write("This app is to increase your <b>productivity</b>.",
+         unsafe_allow_html=True)
 
+st.text_input(label="New item", placeholder="Add new todo...",
+              on_change=add_todo, key="new_todo", label_visibility="collapsed")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=f"{index}_{todo}")
@@ -21,6 +26,3 @@ for index, todo in enumerate(todos):
         functions.write_todos(todos)
         del st.session_state[f"{index}_{todo}"]
         st.rerun()
-
-st.text_input(label="New item", placeholder="Add new todo...",
-              on_change=add_todo, key="new_todo", label_visibility="collapsed")
